43a7bbf8f49724e1fe39fb1cc75b3ecdec5059d2
theFl00f <celeste.ellerby@gmail.com>
2020-05-19 22:57:02 -0400

    removed commented out localStorage functions

--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,12 @@ var express = require('express');
 var Message = require('../models/message')
 var router = express.Router();
 
+//admin only-- delete all messages
+
+// Message.deleteMany({}, function(err) {
+//     if (err) console.log(err);
+//     console.log('successful deletion')
+//   });
 
 router.post('/', async (req, res) => {
     console.log('post received')
